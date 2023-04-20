7997a2a944bdd32d412b7a58b02471d20e8ed19a
Ivan Ustyantsev <vangold915@gmail.com>
2023-04-20 15:06:04 +0700

    filled table for global task number 2

--- homework3/hw3.sql
+++ homework3/hw3.sql
@@ -2,6 +2,9 @@ CREATE DATABASE homework3db;
 
 USE homework3db;
 
+
+### ЗАДАНИЕ 1
+
 CREATE TABLE salespeople
 (
 snum INT PRIMARY KEY NOT NULL UNIQUE,
@@ -125,5 +128,36 @@ FROM orders;
 SELECT *
 FROM customers
 WHERE rating > 100 AND city = "Rome";
+
+### ЗАДАНИЕ 2
+
+CREATE TABLE employee
+(
+id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+firstname VARCHAR(40) NOT NULL,
+surname VARCHAR(40) NOT NULL,
+specialty VARCHAR(40) NOT NULL,
+seniority INT NOT NULL,
+salary INT NOT NULL, 
+age INT NOT NULL
+);
+
+INSERT INTO employee
+(firstname, surname, specialty, seniority, salary, age)
+VALUE
+("Вася", "Васькин", "начальник", 40, 100000, 60),
+("Петя", "Петькин", "начальник", 8, 70000, 30),
+("Катя", "Катькина", "инженер", 2, 70000, 25),
+("Саша", "Сашкин", "инженер", 12, 50000, 35),
+("Иван", "Иванов", "рабочий", 40, 30000, 59),
+("Петр", "Петров", "рабочий", 20, 25000, 40),
+("Сидор", "Сидоров", "рабочий", 10, 20000, 35),
+("Антон", "Антонов", "рабочий", 8, 19000, 28),
+("Юра", "Юркин", "рабочий", 5, 15000, 25),
+("Максим", "Воронин", "рабочий", 2, 11000, 22),
+("Юра", "Галкин", "рабочий", 3, 12000, 24),
+("Люся", "Люськина", "уборщик", 10, 10000, 49);
+
+SELECT * FROM employee;
 
 
