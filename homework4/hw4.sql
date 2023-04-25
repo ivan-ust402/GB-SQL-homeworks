859ccd47dde0f245e31019cc9eec91ea0cfe428f
Ivan Ustyantsev <vangold915@gmail.com>
2023-04-25 15:09:36 +0700

    Completed task 3

--- homework4/hw4.sql
+++ homework4/hw4.sql
@@ -156,3 +156,58 @@ FROM (
 LEFT JOIN AUTO AS t2
 ON t1.MARK != t2.MARK
 GROUP BY t1.MARK;
+
+# Задание 3. 
+-- Даны 2 таблицы, созданные следующим образом:
+-- create table test_a (id INT, test varchar(10));
+-- create table test_b (id INT);
+-- insert into test_a(id, test) values
+-- (10, 'A'),
+-- (20, 'A'),
+-- (30, 'F'),
+-- (40, 'D'),
+-- (50, 'C');
+-- insert into test_b(id) values
+-- (10),
+-- (30),
+-- (50);
+-- Напишите запрос, который вернет строки из таблицы test_a, 
+-- id которых нет в таблице test_b, НЕ используя ключевого слова NOT.
+CREATE TABLE IF NOT EXISTS test_a
+(
+id INT,
+test VARCHAR(10)
+);
+
+CREATE TABLE IF NOT EXISTS test_b
+(
+id INT
+);
+
+INSERT INTO test_a(id, test) 
+VALUES
+(10, 'A'),
+(20, 'A'),
+(30, 'F'),
+(40, 'D'),
+(50, 'C');
+
+INSERT INTO test_b(id) 
+VALUES
+(10),
+(30),
+(50);
+
+SELECT * FROM test_a;
+SELECT * FROM test_b;
+
+SELECT test_a.*
+FROM test_a
+LEFT JOIN test_b
+ON test_a.id = test_b.id
+WHERE test_b.id IS NULL;
+
+
+
+
+
