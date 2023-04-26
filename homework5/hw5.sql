030073ba3fecc09a14bbff3414e8ca38bca4c184
Ivan Ustyantsev <vangold915@gmail.com>
2023-04-26 17:47:20 +0700

    Created table for the first task

--- homework5/hw5.sql
+++ homework5/hw5.sql
@@ -1,2 +1,23 @@
 CREATE DATABASE homework5db;
 USE homework5db;
+
+CREATE TABLE auto_price
+(
+Id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+Name VARCHAR(40) NOT NULL,
+Cost INT NOT NULL
+);
+
+INSERT INTO auto_price
+(Name, Cost)
+VALUES
+("Audi", 52642),
+("Mercedes", 57127),
+("Skoda", 9000),
+("Volvo", 29000),
+("Bentley", 350000),
+("Citroen", 21000),
+("Hummer", 41400),
+("Volkswagen", 21600);
+
+SELECT * FROM auto_price;
