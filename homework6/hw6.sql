f4596543252e1f35aa81c63947fc212ead774799
Ivan Ustyantsev <vangold915@gmail.com>
2023-05-02 22:42:20 +0700

    Completed task 1

--- homework6/hw6.sql
+++ homework6/hw6.sql
@@ -5,7 +5,39 @@ USE homework6db;
 -- Создайте функцию, которая принимает кол-во сек и форматирует их 
 -- в кол-во дней, часов, минут и секунд.
 -- Пример: 123456 ->'1 days 10 hours 17 minutes 36 seconds '
-   
+DELIMITER $$
+CREATE FUNCTION date_from_number(num INT)
+RETURNS VARCHAR(255) DETERMINISTIC
+BEGIN
+DECLARE days INT DEFAULT 0; 
+DECLARE hours INT DEFAULT 0; 
+DECLARE minutes INT DEFAULT 0; 
+DECLARE seconds INT DEFAULT 0;
+DECLARE seconds_in_minute INT DEFAULT 60;
+DECLARE minutes_in_hour INT DEFAULT 60;
+DECLARE hours_in_day INT DEFAULT 24;
+DECLARE result VARCHAR(255) DEFAULT '';
+SET seconds = NUM % seconds_in_minute;
+SET minutes = floor(NUM / seconds_in_minute) % minutes_in_hour;
+SET hours = floor(floor(NUM / seconds_in_minute) / minutes_in_hour) % hours_in_day;
+SET days = floor(NUM / (seconds_in_minute * minutes_in_hour * hours_in_day));
+SET result = concat(result, days);
+SET result = concat(result, " days ");
+SET result = concat(result, hours);
+SET result = concat(result, " hours ");
+SET result = concat(result, minutes);
+SET result = concat(result, " minutes ");
+SET result = concat(result, seconds);
+SET result = concat(result, " seconds");
+RETURN result;
+END $$
+DELIMITER ;
+
+-- DROP FUNCTION date_from_number;
+
+SELECT date_from_number(123456);
+
+
 ### Задание 2
 -- Выведите только четные числа от 1 до 10 включительно.
 -- Пример: 2,4,6,8,10 (можно сделать через шаг + 2: х = 2, х+=2)
